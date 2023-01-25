995bf1b816ab8e6044096ef3d2e2fe1eb1df1346
kg <kevin@griley.app>
2023-01-25 13:37:58 -0800

    changes to user dropdown

--- frontend/src/components/userButton.tsx
+++ frontend/src/components/userButton.tsx
@@ -1,8 +1,23 @@
-
-import { forwardRef } from 'react';
+import React from 'react';
+import { forwardRef, Ref } from 'react';
 import { IconChevronDown } from '@tabler/icons';
-import { Group, Avatar, Text, UnstyledButton } from '@mantine/core';
+import { Group, Avatar, Text, UnstyledButton, createStyles } from '@mantine/core';
 
+const useStyles = createStyles((theme) => ({
+
+    button: {
+        display: 'block',
+        width: '100%',
+        padding: theme.spacing.xs,
+        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+        
+        '&:hover': {
+            backgroundColor:
+                theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
+        },
+    },
+
+}));
 
 interface UserButtonProps extends React.ComponentPropsWithoutRef<'button'> {
     image: string;
@@ -11,41 +26,26 @@ interface UserButtonProps extends React.ComponentPropsWithoutRef<'button'> {
     icon?: React.ReactNode;
 }
 
+export const UserButton: React.FC<UserButtonProps> = forwardRef(
+    ({ image, name, email, icon, ...others }: UserButtonProps,ref: Ref<HTMLButtonElement>) => {
 
-export const UserButton = forwardRef<HTMLButtonElement, UserButtonProps>(
-    ({ image, name, email, icon, ...others }: UserButtonProps, ref) => (
-        <UnstyledButton
-            ref={ref}
-            sx={(theme) => ({
+        const { classes } = useStyles();
 
-                display: 'block',
-                width: '100%',
-                padding: theme.spacing.xs,
-                color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
-
-                '&:hover': {
-                    backgroundColor:
-                        theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
-                },
-
-            })}
-
-            {...others}
-            
-        >
-            <Group>
-                <Avatar src={image} radius="xl" />
-                <div style={{ flex: 1 }}>
-                    <Text size="sm" weight={500}>
-                        {name}
-                    </Text>
-                    <Text color="dimmed" size="xs">
-                        {email}
-                    </Text>
-                </div>
-                {icon || <IconChevronDown size={16} />}
-            </Group>
-
-        </UnstyledButton>
-    )
+        return (
+            <UnstyledButton ref={ref} {...others} className={classes.button}>
+                <Group>
+                    <Avatar src={image} radius="xl" />
+                    <div style={{ flex: 1 }}>
+                        <Text size="sm" weight={500}>
+                            {name}
+                        </Text>
+                        <Text color="dimmed" size="xs">
+                            {email}
+                        </Text>
+                    </div>
+                    {icon || <IconChevronDown size={16} />}
+                </Group>
+            </UnstyledButton>
+        );
+    }
 );
--- frontend/src/components/userMenu.tsx
+++ frontend/src/components/userMenu.tsx
@@ -1,20 +1,23 @@
-import { forwardRef } from 'react';
-import { IconChevronDown } from '@tabler/icons';
-import { Group, Avatar, Text, Menu, UnstyledButton } from '@mantine/core';
-import { IconSettings, IconPhoto, IconMessageCircle, } from '@tabler/icons';
+import { Group, Menu, } from '@mantine/core';
+import { useToggle } from '@mantine/hooks';
+import { IconSettings, IconPhoto, IconMessageCircle, IconChevronUp, IconChevronDown } from '@tabler/icons';
 import { UserButton } from './userButton';
 
 
 export function UserMenu() {
+
+    const [opened, toggle] = useToggle([false, true]);
+
     return (
         <Group position="center">
-            <Menu position="bottom-end" withArrow arrowPosition="center">
+            <Menu width={200} shadow="md" position="bottom-end" opened={opened} onChange={ toggle }>
 
                 <Menu.Target>
                     <UserButton
                         image="https://avatars.githubusercontent.com/u/14338007?v=4"
                         name="Kevin Griley"
                         email="kevin@griley.app"
+                        icon={ opened ? <IconChevronUp size={16} /> : <IconChevronDown size={16} /> }
                     />
                 </Menu.Target>
 
@@ -26,6 +29,7 @@ export function UserMenu() {
                     <Menu.Item icon={<IconPhoto size={14} />}>Gallery</Menu.Item>
 
                 </Menu.Dropdown>
+                
             </Menu>
         </Group>
     );
