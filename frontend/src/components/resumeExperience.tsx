b79a78173bf91aee0f1dabc00fab913530d5be07
kg <kevin@griley.app>
2023-01-26 08:41:02 -0800

    small

--- frontend/src/components/resumeExperience.tsx
+++ frontend/src/components/resumeExperience.tsx
@@ -1,51 +1,105 @@
-import { Container, Title, Paper, List } from '@mantine/core';
+import { Title, createStyles, Space, Divider, Group, Text, List, Container } from '@mantine/core';
+import '../assets/Murecho-font.css';
+
+const useStyles = createStyles((theme) => ({
+
+    f100: {
+        fontFamily: 'Murecho',
+        fontWeight: 100,
+    },
+
+    f200: {
+        fontFamily: 'Murecho',
+        fontWeight: 200,
+    },
+
+    f300: {
+        fontFamily: 'Murecho',
+        fontWeight: 300,
+    },
+
+    f400: {
+      fontFamily: 'Murecho',
+      fontWeight: 400,
+    },
+
+    f500: {
+        fontFamily: 'Murecho',
+        fontWeight: 500,
+    },
+
+    image: {
+        maxHeight: '215px',
+        maxWidth: '215px',
+        height: 'auto',
+        width: 'auto',
+    },
+  
+  }));
 
 export default function ResumeExperience() {
 
+    const { classes, theme } = useStyles();
+
     return (
-        <div className="resume-experience">
+        <Container fluid p={0}>
 
-            <h4>Work Experience:</h4>
+            <Title order={2} className={classes.f400} tt="uppercase">Work Experience:</Title>
+            <Divider size="xs" />
 
-            <h5>CFO & CTO</h5>
-            <h6>Griley Air Freight</h6>
-            <p>July 2016 - Present</p>
-            <ul>
+            <Space h="sm" />
+            <Title order={4} className={classes.f500} tt="capitalize">Chief Technology Officer & Chief Financial Officer</Title>
+            <Space h="xs" />
+            <Group>
+                <Text c="dimmed">Griley Air Freight</Text>
+                <Divider orientation="vertical" />
+                <Text c="dimmed">July 2017 - Present</Text>
+            </Group>
+            <Space h="xs" />
+            <List withPadding>
+                <List.Item>Manage all company expenses and purchases, monitoring accounting costs for different vendors and departments</List.Item>
+                <List.Item>Designed and built an ERP system for the company and migrated it from a legacy system</List.Item>
+                <List.Item>Led a legal case with a massive document discovery request, digitized 30,000 documents and performed data analysis on it</List.Item>
+                <List.Item>Successfully increased company margin from 3% to 22%</List.Item>
+                <List.Item>Designed a web application using Plotly Dash that tracks all expenses and graphs the overtime</List.Item>
+            </List>
+            <Space h="sm" />
 
-                <li>Manage all company expenses and purchases, monitoring accounting costs for different vendors and departments</li>
-                <li>Designed and built an ERP system for the company and migrated it from a legacy system</li>
-                <li>Led a legal case with a massive document discovery request, digitized 30,000 documents and performed data analysis on it</li>
-                <li>Successfully increased company margin from 3% to 22%</li>
-                <li>Designed a web application using Plotly Dash that tracks all expenses and graphs the overtime</li>
+            <Title order={4} className={classes.f500} tt="capitalize">Safety Director</Title>
+            <Space h="xs" />
+            <Group>
+                <Text c="dimmed">Griley Air Freight</Text>
+                <Divider orientation="vertical" />
+                <Text c="dimmed">July 2014 - June 2017</Text>
+            </Group>
+            <Space h="xs" />
+            <List withPadding>
+                <List.Item>Coordinated safety training for all employees and implemented safety protocols</List.Item>
+                <List.Item>Implemented and maintained electronic documentation, increasing efficiency and reducing the need for excessive file keeping</List.Item>
+                <List.Item>Passed BIT Inspection and won California Safest Fleet award multiple times</List.Item>
+                <List.Item>Led efforts to train mechanics to use electronic documentation system effectively</List.Item>
+                <List.Item>Demonstrated in-depth understanding of California and federal regulations in the logistics industry</List.Item>
+                <List.Item>Led and managed a team to achieve company's safety goals</List.Item>
+            </List>
+            <Space h="sm" />
 
-            </ul>
-            <h5>Safety Director</h5>
-            <h6>Griley Air Freight</h6>
-            <p>July 2014 - June 2016</p>
-            <ul>
+            <Title order={4} className={classes.f500} tt="capitalize">Project Manager</Title>
+            <Space h="xs" />
+            <Group>
+                <Text c="dimmed">Griley Air Freight</Text>
+                <Divider orientation="vertical" />
+                <Text c="dimmed">May 2012 - June 2014</Text>
+            </Group>
+            <Space h="xs" />
+            <List withPadding>
+                <List.Item>Led multiple logistics projects, ensuring on-time and on-budget completion</List.Item>
+                <List.Item>Led a project to migrate independent contractors out of the company and move them onto a separate corporation which obtained a federal DOT Freight Broker License</List.Item>
+                <List.Item>Automated dispatching process through serverless function network and web hook integration using a service like zapier</List.Item>
+                <List.Item>Demonstrated strong negotiation and leadership skills, obtained the trust and cooperation of independent contractors</List.Item>
+                <List.Item>Led and managed a team effectively to achieve long-term goals of the company and reduce risk</List.Item>
+            </List>
+            <Space h="sm" />
 
-                <li>Coordinated safety training for all employees and implemented safety protocols</li>
-                <li>Implemented and maintained electronic documentation, increasing efficiency and reducing the need for excessive file keeping</li>
-                <li>Passed BIT Inspection and won California Safest Fleet award multiple times</li>
-                <li>Led efforts to train mechanics to use electronic documentation system effectively</li>
-                <li>Demonstrated in-depth understanding of California and federal regulations in the logistics industry</li>
-                <li>Led and managed a team to achieve company's safety goals</li>
-
-            </ul>
-            <h5>Project Manager</h5>
-            <h6>Griley Air Freight</h6>
-            <p>May 2012 - June 2014</p>
-            <ul>
-
-                <li>Led multiple logistics projects, ensuring on-time and on-budget completion</li>
-                <li>Led a project to migrate independent contractors out of the company and move them onto a separate corporation which obtained a federal DOT Freight Broker License</li>
-                <li>Automated dispatching process through serverless function network and web hook integration using a service like zapier</li>
-                <li>Demonstrated strong negotiation and leadership skills, obtained the trust and cooperation of independent contractors</li>
-                <li>Led and managed a team effectively to achieve long-term goals of the company and reduce risk</li>
-
-            </ul>
-
-
-        </div>
+        </Container>
     )
 }
--- frontend/src/components/resumeHeader.tsx
+++ frontend/src/components/resumeHeader.tsx
@@ -1,4 +1,4 @@
-import { Title, createStyles, Space, Divider, Group, Text, SimpleGrid, Stack, Flex, Image } from '@mantine/core';
+import { Title, createStyles, Space, Divider, Group, Text, SimpleGrid, Stack, Flex, Image, Container } from '@mantine/core';
 import '../assets/Murecho-font.css';
 import { ThemeIcon } from '@mantine/core';
 import { IconPhone, IconMail, IconWorld, IconMapPin} from '@tabler/icons';
@@ -39,89 +39,84 @@ const useStyles = createStyles((theme) => ({
   
   }));
 
-
-
 export default function ResumeHeader() {
-
     const { classes, theme } = useStyles();
 
     return (
-        <div className='resume-header'>
+    <Container fluid p={0}>
+        <SimpleGrid cols={2} mx="md">
 
-            <div>
-                <SimpleGrid cols={2} mx="md">
+            <Stack align="flex-start" justify="flex-start" spacing={0} sx={{ zIndex: 1 }}>
+                <Title size={64} className={classes.f300} sx={{ whiteSpace: 'nowrap' }} tt="capitalize"> Kevin </Title>
+                <Title size={72} className={classes.f400} sx={{ whiteSpace: 'nowrap' }} tt="capitalize"> J. Griley </Title>
+                <Title size={36} className={classes.f300} sx={{ whiteSpace: 'nowrap' }} tt="capitalize"> CTO & CFO </Title>
+            </Stack>
 
-                    <Stack align="flex-start" justify="flex-start" spacing={0} sx={{ zIndex: 1 }}>
-                        <Title size={64} className={classes.f300} sx={{ whiteSpace: 'nowrap' }} > Kevin </Title>
-                        <Title size={72} className={classes.f400} sx={{ whiteSpace: 'nowrap' }} > J. Griley </Title>
-                        <Title size={36} className={classes.f300} sx={{ whiteSpace: 'nowrap' }} > CTO & CFO </Title>
-                    </Stack>
+            <Flex align="center" justify="flex-end">
+                <Image className={classes.image} radius={110} src='/api/static/DSCF9787.jpg'/>
+            </Flex>
 
-                    <Flex align="center" justify="flex-end">
-                        <Image className={classes.image} radius={110} src='/api/static/DSCF9787.jpg'/>
-                    </Flex>
+        </SimpleGrid>
 
-                </SimpleGrid>
+        <Space h="md" />
+        <Divider />
 
-                <Space h="md" />
-                <Divider />
+        <Space h="sm" />
+        <SimpleGrid 
+            ml={theme.spacing.md}
+            mr={theme.spacing.md}
+            spacing="xl" 
+            cols={4}
+            breakpoints={[
+                { maxWidth: theme.breakpoints.lg, cols: 2, spacing: 'md' },
+                { maxWidth: theme.breakpoints.md, cols: 1, spacing: 'sm' },
+                ]}>
+        
+            <Group noWrap>
+                <ThemeIcon radius="lg" size="lg" color="yellow">
+                    <IconPhone /> 
+                </ThemeIcon>
+                <Text sx={{ whiteSpace: 'nowrap' }}> (714) 624-8902 </Text>
+            </Group>
 
-                <Space h="sm" />
-                <SimpleGrid 
-                    ml={theme.spacing.md}
-                    mr={theme.spacing.md}
-                    spacing="xl" 
-                    cols={4}
-                    breakpoints={[
-                        { maxWidth: theme.breakpoints.lg, cols: 2, spacing: 'md' },
-                        { maxWidth: theme.breakpoints.md, cols: 1, spacing: 'sm' },
-                      ]}>
-                
-                    <Group noWrap>
-                        <ThemeIcon radius="lg" size="lg" color="yellow">
-                            <IconPhone /> 
-                        </ThemeIcon>
-                        <Text sx={{ whiteSpace: 'nowrap' }}> (714) 624-8902 </Text>
-                    </Group>
+            <Group noWrap>
+                <ThemeIcon radius="lg" size="lg" color="yellow">
+                    <IconMail /> 
+                </ThemeIcon>
+                <Text sx={{ whiteSpace: 'nowrap' }}> Kevin@Griley.air </Text>
+            </Group>
 
-                    <Group noWrap>
-                        <ThemeIcon radius="lg" size="lg" color="yellow">
-                            <IconMail /> 
-                        </ThemeIcon>
-                        <Text sx={{ whiteSpace: 'nowrap' }}> Kevin@Griley.air </Text>
-                    </Group>
+            <Group noWrap>
+                <ThemeIcon radius="lg" size="lg" color="yellow">
+                    <IconWorld /> 
+                </ThemeIcon>
+                <Text sx={{ whiteSpace: 'nowrap' }}> www.Griley.app </Text>
+            </Group>
 
-                    <Group noWrap>
-                        <ThemeIcon radius="lg" size="lg" color="yellow">
-                            <IconWorld /> 
-                        </ThemeIcon>
-                        <Text sx={{ whiteSpace: 'nowrap' }}> www.Griley.app </Text>
-                    </Group>
+            <Group noWrap>
+                <ThemeIcon radius="lg" size="lg" color="yellow">
+                    <IconMapPin /> 
+                </ThemeIcon>
+                <Text sx={{ whiteSpace: 'nowrap', overflowX: 'hidden'}}> 23136 Anza Ave </Text>
+            </Group>
+        
+        </SimpleGrid>
+        <Space h="sm" />
+        <Divider />
 
-                    <Group noWrap>
-                        <ThemeIcon radius="lg" size="lg" color="yellow">
-                            <IconMapPin /> 
-                        </ThemeIcon>
-                        <Text sx={{ whiteSpace: 'nowrap', overflowX: 'hidden'}}> 23136 Anza Ave </Text>
-                    </Group>
-                
-                </SimpleGrid>
-                <Space h="sm" />
+        <Space h="md" />
+        
+        <Title order={2} className={classes.f400} tt="uppercase">About Me:</Title>
+        <Divider size="xs" />
+        <Space h="sm" />
+        <Text>
+            Highly experienced CTO & CFO with 10 years of experience in the logistics industry. 
+            Proven track record of managing expenses, implementing and maintaining ERP systems, and increasing company margins. 
+            Skilled in data analysis, visual dashboards, and leading projects and teams. 
+            Holds a BA in Business Studies with a focus on Management.
+        </Text>
+        <Space h="sm" />
 
-                <Divider />
-
-                <Space h="md" />
-                
-                <Title italic color="dimmed" order={2} className={classes.f400} >About Me:</Title>
-                <Space h="sm" />
-                <Text>
-                    Highly experienced CTO & CFO with 10 years of experience in the logistics industry. 
-                    Proven track record of managing expenses, implementing and maintaining ERP systems, and increasing company margins. 
-                    Skilled in data analysis, visual dashboards, and leading projects and teams. 
-                    Holds a BA in Business Studies with a focus on Management.
-                </Text>
-
-            </div>
-        </div>
+    </Container>
     )
 }
